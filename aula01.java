babbdce31cd6ea37876f1ebebe1d37250d4c1bc4
thiagokevin <91503622+thiagokevin@users.noreply.github.com>
2023-02-10 01:29:08 +0000

    java_testes

--- aula01.java
+++ aula01.java
@@ -3,6 +3,9 @@ public class aula01
 {
   public static void main(String[] args)
   {
-    System.out.println("ola mundo");
+    float n1 = 6;
+    float n2 = 6;
+    float media = (n1 + n2) /2;
+    System.out.println(media);
   }
 }

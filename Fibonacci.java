d256cc9fa93259abeda9ab8bd9018faa42c7bb00
thiagokevin <91503622+thiagokevin@users.noreply.github.com>
2023-02-17 01:10:35 +0000

    Fibonancci

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+public class Fibonacci 
+{
+    public static void main(String[] agrs)
+    {
+        int num1 =1, num2 = 1;
+
+        System.out.println(num2);
+        System.out.println(num1);
+
+
+
+
+        for(int i = 0; i<8; i++)
+        {
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+        }
+    }
+}
